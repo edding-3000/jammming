5ea8904440045495eea6215e990d19d0441c315a
edding-3000 <sebastian-hauser@outlook.de>
2024-05-24 16:07:45 +0200

    Add to Spotify button and function

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -3,15 +3,27 @@ import Track from '../Track/Track';
 
 function Playlist({ playlistTracks, onTrackButtonClick }) {
     const [playlistName, setPlaylistName] = useState("");
+    const [playlistUris, setPlaylistUris] = useState([]);
 
-
+    // Get playlistname from localstorage when app is loaded
     useEffect(() => {
         setPlaylistName(localStorage.getItem('playlistName') || "");
     }, []);
 
+    // Add URI to playlistUris array when track is added to playlist
+    useEffect(() => {
+        setPlaylistUris(() => playlistTracks.map((playlistTrack) => (playlistTrack.uri)));
+    }, [playlistTracks]);
+
+    // Save playlist name to localstorage and to setter
     const handleChange = ({ target }) => {
         setPlaylistName(target.value);
         localStorage.setItem('playlistName', target.value);
+    }
+
+    // Handle "Add to Spotify click"
+    const addToSpotify = () => {
+        console.log(playlistUris);
     }
 
     return (
@@ -22,6 +34,7 @@ function Playlist({ playlistTracks, onTrackButtonClick }) {
                     <Track trackButtonEvent={onTrackButtonClick} key={index} trackInfos={playlistTrack} button="Remove" />
                 ))}
             </ul>
+            <button onClick={addToSpotify}>Add to Spotify</button>
         </>
     )
 }
--- src/mockData/mockData.js
+++ src/mockData/mockData.js
@@ -3,25 +3,29 @@ const mockData = [
         "name": "name 1",
         "artist": "artist 1",
         "album": "album 1",
-        "id": "ABC123"
+        "id": "ABC123",
+        "uri": "11dFghVXANMlKmJXsNCbNl"
     },
     {
         "name": "hallo",
         "artist": "artist 2",
         "album": "album 2",
-        "id": "DEF456"
+        "id": "DEF456",
+        "uri": "12dFghVXANMlKmJXsNCbNl"
     },
     {
         "name": "was geht ab?",
         "artist": "artist 3",
         "album": "album 3",
-        "id": "GHI789"
+        "id": "GHI789",
+        "uri": "13dFghVXANMlKmJXsNCbNl"
     },
     {
         "name": "u there?",
         "artist": "artist 4",
         "album": "album 4",
-        "id": "JKL012"
+        "id": "JKL012",
+        "uri": "14dFghVXANMlKmJXsNCbNl"
     }
 ];
 
